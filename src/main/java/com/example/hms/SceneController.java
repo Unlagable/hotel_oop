d762e8782b2b815d4e0ae200ca892fd8a3a8ec1d
Unlagable <engqwerty43@gmail.com>
2024-03-25 00:04:35 +0700

    ok

--- src/main/java/com/example/hms/SceneController.java
+++ src/main/java/com/example/hms/SceneController.java
@@ -1,19 +1,29 @@
 package com.example.hms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SceneController {
-
     private Stage stage;
     private Scene scene;
+
     private Parent root;
+
+
+
 
     public void userLogin(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("userlogin-view.fxml"));
@@ -39,5 +49,12 @@ public class SceneController {
         stage.setTitle("Hotel Login");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void writeFile(ArrayList<HotelRoomTable> hotel, PrintWriter pw){
+        for (HotelRoomTable room:hotel) {
+            pw.println(room.getRoomNo()+";"+room.getType()+";"+room.getCapacity()+";"+room.getPrice()+";"+room.getStatus());
+        }
+        pw.close();
     }
 }
--- src/main/java/com/example/hms/UserDetail.java
+++ src/main/java/com/example/hms/UserDetail.java
@@ -1,10 +1,18 @@
 package com.example.hms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UserDetail {
     private String username;
     private String password;
     private String email;
     private String phoneNum;
+    UserDetail(){}
     UserDetail(String username, String password, String email, String phoneNum){
         this.username = username;
         this.password = password;
@@ -43,4 +51,76 @@ public class UserDetail {
     public String getPhoneNum() {
         return phoneNum;
     }
+
+    @Override
+    public String toString() {
+        return "UserDetail{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                '}';
+    }
+    static Scanner inp = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<UserDetail> user = new ArrayList<>();
+        File fl = new File("userDetail.txt");
+//        FileOutputStream fos = new FileOutputStream(fl,false);
+//        PrintWriter pw = new PrintWriter(fos);
+//        writeFile(hotel,pw);
+
+        Scanner cs  = new Scanner(fl);
+        cs.useDelimiter(";|\r\n");
+
+
+
+//        while (cs.hasNextLine()){
+//            UserDetail s = new UserDetail();
+//            s.setUsername(cs.next());
+//            s.setPassword(cs.next());
+//            s.setEmail(cs.next());
+//            s.setPhoneNum(cs.next());
+//            cs.nextLine();
+//            user.add(s);
+//        }
+
+
+        while (true){
+//            menu();
+            int op = inp.nextInt();
+            switch (op){
+                case 1:
+//                    addRoom(hotel);
+                    break;
+                case 2:
+//                    printAll(hotel);
+                    break;
+                case 3:
+                    System.out.print("Enter Room Number: ");
+//                    String num = inp.next();
+//                    searchRoomNumber(hotel,num);
+                    break;
+                case 4:
+                    System.out.print("status: ");
+//                    String status = inp.next();
+//                    sortByStatus(hotel,status);
+                    break;
+                case 7:
+                    System.out.print("Enter Student's ID: ");
+//                    String rnum = inp.next();
+//                    deleteRoom(hotel,rnum);
+                    break;
+                case 0:
+//                    FileOutputStream fos = new FileOutputStream(fl,false);
+//                    PrintWriter pw = new PrintWriter(fos);
+//                    writeFile(hotel,pw);
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input.Try again");
+            }
+        }
+    }
+
+
 }
+
