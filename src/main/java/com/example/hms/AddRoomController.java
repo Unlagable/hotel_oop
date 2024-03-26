7162428381b5aa67b606dbd16e26c33be47de96b
Unlagable <engqwerty43@gmail.com>
2024-03-26 23:40:40 +0700

    Master

--- src/main/java/com/example/hms/AddRoomController.java
+++ src/main/java/com/example/hms/AddRoomController.java
@@ -1,20 +1,27 @@
 package com.example.hms;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class AddRoomController {
+public class AddRoomController implements Initializable {
     @FXML
-    protected TextField txt_number;
+    protected TextField txt_RoomNo;
     @FXML
     protected TextField txt_type;
     @FXML
@@ -22,61 +29,100 @@ public class AddRoomController {
     @FXML
     protected TextField txt_price;
     @FXML
-    protected TextField txt_status;
-
+    protected ChoiceBox<String> choiceBox_Status;
     @FXML
     protected Label lb_mess;
-    @FXML
-    protected Label lb_mess_error;
-    @FXML
-    protected void onSubmit(Event e){
-        if(txt_type.getText().length()>0 &&
-                txt_number.getText().length()>0) {
-//            User u = new User(txt_name.getText(),txt_gen.getText(),
-//                    Integer.parseInt(txt_age.getText()));
-            Room u = new Room();
-//            u.name = txt_name.getText();
-            u.setNumber(Integer.parseInt(txt_number.getText()));
-            u.setType(txt_type.getText());
-            u.setCapacity(txt_capacity.getText());
-            u.setPrice(Integer.parseInt(txt_price.getText()));
-            u.setStatus(txt_status.getText());
-            txt_number.setText("");
+//    @FXML
+//    protected Label lb_mess_error;
+    private String[] status = {"Available","Unavailable"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choiceBox_Status.getItems().addAll(status);
+    }
+
+    public void cancelBtn(ActionEvent event){
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
+
+    public void addRoomBtn(ActionEvent event){
+        if (!txt_RoomNo.getText().isEmpty() && !txt_type.getText().isEmpty() && !txt_capacity.getText().isEmpty() && !txt_price.getText().isEmpty() && !choiceBox_Status.getSelectionModel().isEmpty()){
+            HotelRoomTable room = new HotelRoomTable(txt_RoomNo.getText(),txt_type.getText(),txt_capacity.getText(),txt_price.getText(),choiceBox_Status.getValue());
+            txt_RoomNo.setText("");
             txt_type.setText("");
             txt_capacity.setText("");
             txt_price.setText("");
-            txt_status.setText("");
+            choiceBox_Status.setValue("");
 
-            Apps.ulist.add(u);
-            System.out.println("Save Success !");
-            lb_mess.setText("Save Success !");
-            lb_mess_error.setText("");
-//        u.display();
-//            System.out.println("submit" + txt_name.getText() + " " + txt_age.getText());
-        }else{
-            lb_mess_error.setText("you should input name and age!");
-            lb_mess.setText("");
-            System.out.println("you should input name and age!");
-        }
-    }
 
-    @FXML
-    protected  void onShowList(Event event) throws IOException {
-       /*
-        System.out.println("List User :");
-        for (int i = 0; i <ulist.size() ; i++) {
-           User u = ulist.get(i);
-           u.display();
+
+            Main.roomList.add(room);
+            System.out.println("Add Succeed");
+            lb_mess.setText("Room Successfully Added");
+            System.out.println(room);
+        }else {
+            lb_mess.setText("Please Enter All Information");
         }
 
-        //  */
-        Parent root = FXMLLoader.load(Apps.class.getResource("home-view.fxml"));
-        Stage stage = (Stage) ( (Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setWidth(stage.getWidth());
-        stage.setHeight(stage.getHeight());
-        stage.setTitle("List Room");
-        stage.setScene(scene);
-        stage.show();
     }
+
+
+
+
+//    @FXML
+//    protected void onSubmit(Event e){
+//        if(txt_type.getText().length()>0 &&
+//                txt_RoomNo.getText().length()>0) {
+////            User u = new User(txt_name.getText(),txt_gen.getText(),
+////                    Integer.parseInt(txt_age.getText()));
+//            Room u = new Room();
+////            u.name = txt_name.getText();
+//            u.setNumber(Integer.parseInt(txt_RoomNo.getText()));
+//            u.setType(txt_type.getText());
+//            u.setCapacity(txt_capacity.getText());
+//            u.setPrice(Integer.parseInt(txt_price.getText()));
+//            u.setStatus(txt_status.getText());
+//            txt_RoomNo.setText("");
+//            txt_type.setText("");
+//            txt_capacity.setText("");
+//            txt_price.setText("");
+//            txt_status.setText("");
+//
+//            Apps.ulist.add(u);
+//            System.out.println("Save Success !");
+//            lb_mess.setText("Save Success !");
+//            lb_mess_error.setText("");
+////        u.display();
+////            System.out.println("submit" + txt_name.getText() + " " + txt_age.getText());
+//        }else{
+//            lb_mess_error.setText("you should input name and age!");
+//            lb_mess.setText("");
+//            System.out.println("you should input name and age!");
+//        }
+//    }
+//
+//    @FXML
+//    protected  void onShowList(Event event) throws IOException {
+//       /*
+//        System.out.println("List User :");
+//        for (int i = 0; i <ulist.size() ; i++) {
+//           User u = ulist.get(i);
+//           u.display();
+//        }
+//
+//        //  */
+//        Parent root = FXMLLoader.load(Apps.class.getResource("home-view.fxml"));
+//        Stage stage = (Stage) ( (Node)event.getSource()).getScene().getWindow();
+//        Scene scene = new Scene(root);
+//        stage.setWidth(stage.getWidth());
+//        stage.setHeight(stage.getHeight());
+//        stage.setTitle("List Room");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+
+
+
+
 }
--- src/main/java/com/example/hms/ListRoomController.java
+++ src/main/java/com/example/hms/ListRoomController.java
@@ -2,16 +2,17 @@ package com.example.hms;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,67 +20,78 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ListRoomController implements Initializable {
-    ObservableList<Room>  list= FXCollections.observableArrayList();
+    ObservableList<HotelRoomTable>  list = FXCollections.observableArrayList();
     @FXML
-    private TableView<Room> list_user_tableview;
+    private TableView<HotelRoomTable> list_user_tableview;
     @FXML
-    private TableColumn<Room,String> type;
+    private TableColumn<HotelRoomTable,String> roomNo;
     @FXML
-    private TableColumn<Room,Integer> number;
+    private TableColumn<HotelRoomTable,String> type;
     @FXML
-    private TableColumn<Room,String> capacity;
-
+    private TableColumn<HotelRoomTable,String> capacity;
     @FXML
-    private TableColumn<Room,Integer> price;
+    private TableColumn<HotelRoomTable,String> price;
     @FXML
-    private TableColumn<Room,String> status;
+    private TableColumn<HotelRoomTable,String> status;
 
     private void initeCols(){
-        number.setCellValueFactory(new PropertyValueFactory<>("number"));
+        roomNo.setCellValueFactory(new PropertyValueFactory<>("roomNo"));
         type.setCellValueFactory(new PropertyValueFactory<>("type"));
         capacity.setCellValueFactory(new PropertyValueFactory<>("capacity"));
         price.setCellValueFactory(new PropertyValueFactory<>("price"));
         status.setCellValueFactory(new PropertyValueFactory<>("status"));
     }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initeCols();
-        loadDara();
+        loadData();
+
     }
 
-    private void loadDara(){
-        list.removeAll(list); // should remove
-        /*
-        list.addAll(
-                new User("sok","male",20),
-                new User("sao","male",25),
-                new User("jonh","male",30),
-                new User("dara","male",22)
-        );
-        //*/
-       list.addAll(Apps.ulist);
-       list_user_tableview.setItems(list);
+    private void loadData(){
+//        list.removeAll(list); // should remove
+//        /*
+//        list.addAll(
+//                new User("sok","male",20),
+//                new User("sao","male",25),
+//                new User("jonh","male",30),
+//                new User("dara","male",22)
+//        );
+//        //*/
+//       list.addAll(Apps.ulist);
+        HotelRoomTable r1 = new HotelRoomTable("1","11","2","3","4");
+        HotelRoomTable r2 = new HotelRoomTable("11","12","22","33","44");
+        list.addAll(Main.roomList);
+        list_user_tableview.setItems(list);
     }
 
     @FXML
     protected  void onAddUser(Event event) throws IOException {
-        Parent root = FXMLLoader.load(Apps.class.getResource("add-room-view.fxml"));
-        Stage stage = (Stage) ( (Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
+//        Parent root1 = FXMLLoader.load(Apps.class.getResource("addroom-view.fxml"));
+//        Stage stage = (Stage) ( (Node)event.getSource()).getScene().getWindow();
+//        Scene scene = new Scene(root1);
+//        stage.setTitle("Add Room");
+//        stage.setWidth(stage.getWidth());
+//        stage.setHeight(stage.getHeight());
+//        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addroom-view.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
         stage.setTitle("Add Room");
-        stage.setWidth(stage.getWidth());
-        stage.setHeight(stage.getHeight());
-        stage.setScene(scene);
+        stage.setScene(new Scene(root1));
+        stage.initModality(Modality.APPLICATION_MODAL);
 
-        stage.show();
+        stage.showAndWait();
     }
 
     @FXML
-    protected  void onTest(Event event) throws IOException {
+    protected  void onTest(ActionEvent event) throws IOException {
 
-        list.clear();
-        list.addAll(Apps.ulist);
-        list_user_tableview.setItems(list);
+
+//        list.clear();
+//        list.addAll(Apps.ulist);
+        list_user_tableview.getItems().add(Main.roomList.get(0));
 
     }
 
